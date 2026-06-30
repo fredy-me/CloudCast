0ba8d3f9c5dd916e0d55fbc1bd64e9ff4ea0a195
fredy-me <fredymelkizedek@gmail.com>
2026-06-30 21:26:51 +0300

    Implement open LoginActivity and MainActivity

--- app/src/main/java/com/busaradigital/cloudcast/RegisterActivity.java
+++ app/src/main/java/com/busaradigital/cloudcast/RegisterActivity.java
@@ -1,24 +1,39 @@
 package com.busaradigital.cloudcast;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button btnRegister;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(v -> {
+            // Handle button click here
+            Intent openLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(openLogin);
+            finish();
+
+        });
+
+        btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(v -> {
+            // Handle button click here
+            Intent openRegister = new Intent(RegisterActivity.this, MainActivity.class);
+            startActivity(openRegister);
+            finish();
+
         });
     }
 }
